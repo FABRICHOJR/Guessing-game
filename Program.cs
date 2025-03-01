df2d952b4758871a7bd6d8c1fc0a095b0baf1113
Fabrizio <118023919+FABRICHOJR@users.noreply.github.com>
2025-03-01 13:29:02 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,49 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, c sharp");
+Random random = new Random();
+string guessN = "";
+int randomNumber = random.Next(1, 100);
+int guessNumber = 0;
+bool adivinaste = false;
+
+Console.WriteLine("GUESSING GAME BY FABRIZIO RADICCI");
+
+
+while (!adivinaste)
+{
+    Console.WriteLine("Enter your guess number: ");
+    guessN = Console.ReadLine()!;
+
+    if (int.TryParse(guessN, out guessNumber))
+    {
+        if (guessNumber == randomNumber)
+        {
+            adivinaste = true;
+            Console.WriteLine("Bravisimo!!! GANASTE!");
+        }
+        else if (guessNumber < randomNumber)
+        {
+            Console.WriteLine(" es un numero mas alto");
+
+        }
+        else
+        {
+            Console.WriteLine("es un numero mas bajo ");
+        }
+    }
+else
+        {
+            Console.WriteLine("Invalid number, try again");
+
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
 
